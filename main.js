a0f3a820365a4cb3e2a5c88e3419748f4b9ffd40
carolbadre <119211831+carolbadre@users.noreply.github.com>
2023-04-16 18:37:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function getRandomColor() {
-  var hue = Math.floor(Math.random() * 60);
-  var saturation = Math.floor(Math.random() * 25);
-  var brightness = Math.floor(Math.random() * 15) + 75;
-  return '
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfobject/2.2.5/pdfobject.min.js"></script>
+<script>
+  document.querySelectorAll('.pdf-grid a').forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      var pdfUrl = this.href;
+      PDFObject.embed(pdfUrl, "#pdf-viewer");
+      document.querySelector('#pdf-modal').classList.add('open');
+    });
+  });
+
+  document.querySelector('#pdf-modal .close').addEventListener('click', function() {
+    document.querySelector('#pdf-modal').classList.remove('open');
+    document.querySelector('#pdf-viewer').innerHTML = '';
+  });
+</script>
+
+// Other code for other sections of your portfolio page can go here
