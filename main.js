e95fc67fffdef5c81e0c84eaca22a4c3f3904a7d
carolbadre <119211831+carolbadre@users.noreply.github.com>
2023-04-16 11:58:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function getRandomColor() {
+  var hue = Math.floor(Math.random() * 60);
+  var saturation = Math.floor(Math.random() * 25);
+  var brightness = Math.floor(Math.random() * 15) + 75;
+  return '
