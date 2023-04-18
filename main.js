ab5236f03a9b32d1007b970a44c5ebd773b115be
carolbadre <119211831+carolbadre@users.noreply.github.com>
2023-04-18 12:21:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,59 +11,62 @@
 
   const navLinks = document.querySelectorAll(".nav__link");
 
-const colors = [
-  "#b8c4c6",
-  "#e6efc9",
-  "#abb2cc",
-  "#b6daf4",
-  "#e4cce1",
-  "#c0dcb4",
-  "#e2b9ae",
-  "#b8d0f1",
-  "#eed5e0",
-  "#d8e2af",
-  "#ebc6ac",
-  "#d7b6b7",
-  "#f6e8c6",
-  "#ebf1e6",
-  "#e7e4f0",
-  "#dbc6b1",
-  "#aecf9b",
-  "#f0cce1",
-  "#daddea",
-  "#f8e6e3",
-  "#f1e8ea",
-  "#f4d6e8",
-  "#dff0dc",
-  "#d1e9fe",
-  "#dae8f3",
-  "#eddeea",
-  "#e3e2ef",
-  "#d3dbed",
-  "#e9f1d2",
-  "#faf9d9",
-  "#ecd1f0",
-  "#eee1ec",
-  "#dee7f8",
-  "#eec7d5",
-  "#f3ece3",
-  "#dcecda",
-  "#f1f9db",
-  "#fae7d4",
-  "#edf9db",
-  "#d6d7e9",
-  "#f1ece4",
-  "#edf5e8"
-];
+  const colors = [
+    "#b8c4c6",
+    "#e6efc9",
+    "#abb2cc",
+    "#b6daf4",
+    "#e4cce1",
+    "#c0dcb4",
+    "#e2b9ae",
+    "#b8d0f1",
+    "#eed5e0",
+    "#d8e2af",
+    "#ebc6ac",
+    "#d7b6b7",
+    "#f6e8c6",
+    "#ebf1e6",
+    "#e7e4f0",
+    "#dbc6b1",
+    "#aecf9b",
+    "#f0cce1",
+    "#daddea",
+    "#f8e6e3",
+    "#f1e8ea",
+    "#f4d6e8",
+    "#dff0dc",
+    "#d1e9fe",
+    "#dae8f3",
+    "#eddeea",
+    "#e3e2ef",
+    "#d3dbed",
+    "#e9f1d2",
+    "#faf9d9",
+    "#ecd1f0",
+    "#eee1ec",
+    "#dee7f8",
+    "#eec7d5",
+    "#f3ece3",
+    "#dcecda",
+    "#f1f9db",
+    "#fae7d4",
+    "#edf9db",
+    "#d6d7e9",
+    "#f1ece4",
+    "#edf5e8"
+  ];
 
-navLinks.forEach((link) => {
-  link.addEventListener("mouseenter", () => {
-    const colorIdx = Math.floor(Math.random() * colors.length);
-    const color = colors[colorIdx];
-    link.style.setProperty("--hover-color", color);
-  });
+  const seconds = new Date().getSeconds();
+  const colorIdx = seconds % colors.length;
 
-  link.addEventListener("mouseleave", () => {
-    link.style.setProperty("--hover-color", "");
+  navLinks.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+      const color = colors[colorIdx];
+      link.style.setProperty("--hover-color", color);
+    });
+
+    link.addEventListener("mouseleave", () => {
+      link.style.setProperty("--hover-color", "");
+    });
   });
-});
+</script>
